ef5b92a8811bb5b8894d0d0aee9140db6878c43a
krisha-3 <kri385385@gmail.com>
2026-06-11 20:26:35 +0530

    Change the layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -5,13 +5,13 @@ import { Outlet } from "react-router";
 
 function Layout() {
   return (
-    <div className="h-screen flex flex-col">
+    <div className="h-screen flex flex-col overflow-hidden">
       <Navbar />
 
-      <div className="flex flex-1">
+      <div className="flex flex-1 overflow-hidden">
         <Sidebar />
 
-        <main className="flex-1 p-4">
+        <main className="flex-1 p-4 overflow-y-auto">
           <Outlet />
         </main>
       </div>
